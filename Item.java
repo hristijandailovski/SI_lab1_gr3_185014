9a64ec91f7d2950daa8ad28d0c11e9a44c6a934a
Hristijan Dailovski <hristijan.dailovski@outlook.com>
2020-03-31 22:32:23 +0200

    taxReturn method finished

--- Item.java
+++ Item.java
@@ -7,15 +7,58 @@ class Item {
 	char tipNaDanok;
 	//TODO constructor
 
+	public Item(int id, String name, double price, char tipNaDanok ="B") {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.tipNaDanok = tipNaDanok;
+	}
 
 	//TODO setters and getters
 
-	double taxReturn () {
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public char getTipNaDanok() {
+		return tipNaDanok;
+	}
+
+	public void setTipNaDanok(char tipNaDanok) {
+		this.tipNaDanok = tipNaDanok;
+	}
+
+	double getTaxReturn () {
 		//TODO
+		double ddv=0
+		if(tipNaDanok=='А'){
+			ddv=this.price * 0.18;
+		}
+		if(tipNaDanok=='Б'){
+			ddv=this.price * 0.05;
+		}
+		return ddv*0.15;
 	}
 
-	public static void main(String[] args) {
-
-	}
 
 }
